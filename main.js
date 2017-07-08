f98dd5b6ceae6bb551f26131728be49cf2b499bf
Alexa Kovachevich <alexakovachevich12@gmail.com>
2017-07-08 01:05:14 -0400

    commented out js test material and added back cartoon duck image

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-	alert("Are you feeling lucky today?");
+	// alert("Are you feeling lucky today?");
 
-	$( "#clickme" ).click(function() {
-  		$( "#book" ).animate({
-   		 	left: "+=50",
-  		}, 5000, function() {
-    // Animation complete.
-  		});
-	});
+	// $( "#clickme" ).click(function() {
+ //  		$( "#book" ).animate({
+ //   		 	left: "+=50",
+ //  		}, 5000, function() {
+ //    // Animation complete.
+ //  		});
+	// });
 });
